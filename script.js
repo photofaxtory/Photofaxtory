3348d3333bfe27ce616d7d31ff239111abaa43c9
photofaxtory <131462450+photofaxtory@users.noreply.github.com>
2023-10-22 15:55:47 +0530

    all files added

---
+++ script.js
@@ -0,0 +1,138 @@
+const scroll = new LocomotiveScroll({
+  el: document.querySelector(".home"),
+  smooth: true,
+});
+
+gsap.from("#navlink", {
+  stagger: 0.2,
+  y: 10,
+  duration: 1,
+  ease: Power2,
+  opacity: 0,
+});
+
+Shery.textAnimate(".headings h1", {
+  style: 2,
+  y: 10,
+  delay: 0.1,
+  duration: 2,
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  multiplier: 0.1,
+});
+
+gsap.from("#anim2", {
+  y: 50,
+  stagger: 0.3,
+  opacity: 0,
+  ease: Expo,
+  duration: 1,
+});
+
+Shery.imageEffect(".imageandtext img", {
+  style: 3,
+  config: {
+    uFrequencyX: { value: 12, range: [0, 100] },
+    uFrequencyY: { value: 12, range: [0, 100] },
+    uFrequencyZ: { value: 23.14, range: [0, 100] },
+    geoVertex: { range: [1, 64], value: 32 },
+    zindex: { value: -9996999, range: [-9999999, 9999999] },
+    aspect: { value: 0.8 },
+    gooey: { value: false },
+    infiniteGooey: { value: false },
+    growSize: { value: 4, range: [1, 15] },
+    durationOut: { value: 1, range: [0.1, 5] },
+    durationIn: { value: 1.5, range: [0.1, 5] },
+    displaceAmount: { value: 0.5 },
+    masker: { value: true },
+    maskVal: { value: 1.2, range: [1, 5] },
+    scrollType: { value: 0 },
+    noEffectGooey: { value: true },
+    onMouse: { value: 1 },
+    noise_speed: { value: 0.2, range: [0, 10] },
+    metaball: { value: 0.2, range: [0, 2] },
+    discard_threshold: { value: 0.5, range: [0, 1] },
+    antialias_threshold: { value: 0.002, range: [0, 0.1] },
+    noise_height: { value: 0.5, range: [0, 2] },
+    noise_scale: { value: 10, range: [0, 100] },
+  },
+});
+Shery.imageEffect(".portraitimagewrapper img", {
+  style: 5,
+  config: {
+    a: { value: 3.97, range: [0, 30] },
+    b: { value: -0.83, range: [-1, 1] },
+    zindex: { value: -9996999, range: [-9999999, 9999999] },
+    aspect: { value: 0.7999694959200793 },
+    gooey: { value: false },
+    infiniteGooey: { value: false },
+    growSize: { value: 4, range: [1, 15] },
+    durationOut: { value: 1, range: [0.1, 5] },
+    durationIn: { value: 1.5, range: [0.1, 5] },
+    displaceAmount: { value: 0.5 },
+    masker: { value: true },
+    maskVal: { value: 1.23, range: [1, 5] },
+    scrollType: { value: 0 },
+    geoVertex: { range: [1, 64], value: 1 },
+    noEffectGooey: { value: true },
+    onMouse: { value: 1 },
+    noise_speed: { value: 0.2, range: [0, 10] },
+    metaball: { value: 0.2, range: [0, 2] },
+    discard_threshold: { value: 0.5, range: [0, 1] },
+    antialias_threshold: { value: 0.002, range: [0, 0.1] },
+    noise_height: { value: 0.5, range: [0, 2] },
+    noise_scale: { value: 10, range: [0, 100] },
+  },
+});
+Shery.imageEffect(".pictures img", {
+  style: 5,
+  config: {
+    a: { value: 3.97, range: [0, 30] },
+    b: { value: -0.83, range: [-1, 1] },
+    zindex: { value: -9996999, range: [-9999999, 9999999] },
+    aspect: { value: 0.7999694959200793 },
+    gooey: { value: false },
+    infiniteGooey: { value: false },
+    growSize: { value: 4, range: [1, 15] },
+    durationOut: { value: 1, range: [0.1, 5] },
+    durationIn: { value: 1.5, range: [0.1, 5] },
+    displaceAmount: { value: 0.5 },
+    masker: { value: true },
+    maskVal: { value: 1.23, range: [1, 5] },
+    scrollType: { value: 0 },
+    geoVertex: { range: [1, 64], value: 1 },
+    noEffectGooey: { value: true },
+    onMouse: { value: 1 },
+    noise_speed: { value: 0.1, range: [0, 10] },
+    metaball: { value: 0.2, range: [0, 2] },
+    discard_threshold: { value: 0.5, range: [0, 1] },
+    antialias_threshold: { value: 0.002, range: [0, 0.1] },
+    noise_height: { value: 0.5, range: [0, 2] },
+    noise_scale: { value: 10, range: [0, 100] },
+  },
+});
+gsap.from(".imageandtext img",{
+    y: "70",
+    opacity: 0,
+    duration: 2,
+    ease: Expo.easeInOut
+})
+
+document.querySelector(".ftext button")
+.addEventListener("mouseover", function(){ 
+    gsap.to("footer video",{
+        opacity: 1,
+        duration:1.5,
+        ease: Power4
+    })
+    
+} )
+document.querySelector(".ftext button")
+.addEventListener("mouseleave", function(){ 
+    gsap.to("footer video",{
+        opacity: 0,
+        duration:1.5,
+        ease: Power4
+    })
+    
+} )
+
